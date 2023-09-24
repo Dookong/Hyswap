b2020636737bacff33d3813e74209046e34ea8ba
uuccu <sungu0804@gmail.com>
2023-09-25 01:37:29 +0900

    Update HySwapRouterTest.sol

--- test/HySwapRouterTest.sol
+++ test/HySwapRouterTest.sol
@@ -8,36 +8,157 @@ import "../src/contracts/Factory.sol";
 
 
 contract HySwapRouterTest is Test {
-    ERC20Mintable public token0;
-    ERC20Mintable public token1;
+    ERC20Mintable public tokenA;
+    ERC20Mintable public tokenB;
 
     HySwapRouter router;
     Factory factory;
 
     function setUp() public {
-        token0 = new ERC20Mintable("A", "TKA");
-        token1 = new ERC20Mintable("B", "TKB");
+        tokenA = new ERC20Mintable("A", "TKA");
+        tokenB = new ERC20Mintable("B", "TKB");
+
+        (tokenA, tokenB) = address(tokenA) < address(tokenB) ? (tokenA, tokenB) : (tokenB, tokenA);
 
         factory = new Factory(address(this));
         router = new HySwapRouter(address(factory));
 
-        token0.mint(20 ether, address(this));
-        token1.mint(20 ether, address(this));
+        tokenA.mint(20 ether, address(this));
+        tokenB.mint(20 ether, address(this));
     }
+
+    function encodeError(string memory error)
+        internal
+        pure
+        returns (bytes memory encoded)
+    {
+        encoded = abi.encodeWithSignature(error);
+    }
+
 
     function testAddLiquidity() public {
-        token0.approve(address(router), 10 ether);
-        token1.approve(address(router), 10 ether);
+        tokenA.approve(address(router), 1 ether);
+        tokenB.approve(address(router), 1 ether);
 
-        router.addLiquidity(address(token0), address(token1), 10 ether, 10 ether, 0, 0, address(this));
+        router.addLiquidity(address(tokenA), address(tokenB), 1 ether, 1 ether, 1 ether, 1 ether, address(this));
 
-        HySwapPair pair = HySwapPair(factory.getPair(address(token0), address(token1)));
-        assertEq(pair.balanceOf(address(this)), 9999999999999999000); // minimum liquidity 1000
+        HySwapPair pair = HySwapPair(factory.getPair(address(tokenA), address(tokenB)));
+        assertEq(pair.balanceOf(address(this)), 999999999999999000); // minimum liquidity 1000
 
-        assertEq(token0.balanceOf(address(this)), 10 ether);
-        assertEq(token1.balanceOf(address(this)), 10 ether);
+        assertEq(tokenA.balanceOf(address(this)), 19 ether);
+        assertEq(tokenB.balanceOf(address(this)), 19 ether);
 
-        assertEq(token0.balanceOf(address(pair)), 10 ether);
-        assertEq(token1.balanceOf(address(pair)), 10 ether);
+        assertEq(tokenA.balanceOf(address(pair)), 1 ether);
+        assertEq(tokenB.balanceOf(address(pair)), 1 ether);
     }
+
+    function testAddLiquidityNoPair() public {
+        tokenA.approve(address(router), 1 ether);
+        tokenB.approve(address(router), 1 ether);
+
+        (uint256 amountA, uint256 amountB, uint256 liquidity) = router
+        .addLiquidity(address(tokenA), address(tokenB), 1 ether, 1 ether, 1, 1, address(this));
+
+        assertEq(amountA, 1 ether);
+        assertEq(amountB, 1 ether);
+        assertEq(liquidity, 1 ether - 1000);
+
+        HySwapPair pair = HySwapPair(factory.getPair(address(tokenA), address(tokenB)));
+
+        assertEq(tokenA.balanceOf(address(pair)), 1 ether);
+        assertEq(tokenB.balanceOf(address(pair)), 1 ether);
+
+        assertEq(tokenA.balanceOf(address(this)), 19 ether);
+        assertEq(tokenB.balanceOf(address(this)), 19 ether);
+
+        assertEq(pair.token0(), address(tokenA));
+        assertEq(pair.token1(), address(tokenB));
+
+        assertEq(pair.totalSupply(), 1 ether);
+        assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
+    }
+
+    function testAddLiquidityCheckOptimal() public {
+        HySwapPair pair = HySwapPair(factory.createPair(address(tokenA), address(tokenB)));
+
+        assertEq(pair.token0(), address(tokenA));
+        assertEq(pair.token1(), address(tokenB));
+
+        tokenA.transfer(address(pair), 1 ether);
+        tokenB.transfer(address(pair), 2 ether);
+        uint256 _liquidity = pair.mint(address(this));
+
+        assertEq(_liquidity, 1414213562373094048);
+
+        tokenA.approve(address(router), 1 ether);
+        tokenB.approve(address(router), 2 ether);
+
+        (uint256 amountA, uint256 amountB, uint256 liquidity) = router
+        .addLiquidity(address(tokenA), address(tokenB), 1 ether, 2 ether, 1 ether, 1.9 ether, address(this));
+
+        assertEq(amountA, 1 ether);
+        assertEq(amountB, 2 ether);
+        assertEq(liquidity, 1414213562373095048);
+        assertEq(_liquidity, liquidity - 1000);
+    }
+
+    function testAddLiquidityAmountBOptimalIsTooLow() public {
+        HySwapPair pair = HySwapPair(factory.createPair(address(tokenA), address(tokenB)));
+
+        assertEq(pair.token0(), address(tokenA));
+        assertEq(pair.token1(), address(tokenB));
+
+        tokenA.transfer(address(pair), 5 ether);
+        tokenB.transfer(address(pair), 10 ether);
+        uint256 _liquidity = pair.mint(address(this));
+
+
+        tokenA.approve(address(router), 1 ether);
+        tokenB.approve(address(router), 2 ether);
+
+        vm.expectRevert(encodeError("InsufficientBAmount()")); 
+        (uint256 amountA, uint256 amountB, uint256 liquidity) = router
+        .addLiquidity(address(tokenA), address(tokenB), 1 ether, 2 ether, 1 ether, 2 ether, address(this));
+    }
+
+    function testAddLiquidityAmountBOptimalTooHighAmountATooLow() public {
+        HySwapPair pair = HySwapPair(factory.createPair(address(tokenA), address(tokenB)));
+
+        assertEq(pair.token0(), address(tokenA));
+        assertEq(pair.token1(), address(tokenB));
+
+        tokenA.transfer(address(pair), 10 ether);
+        tokenB.transfer(address(pair), 5 ether);
+        uint256 _liquidity = pair.mint(address(this));
+
+        tokenA.approve(address(router), 2 ether);
+        tokenB.approve(address(router), 1 ether);
+
+        vm.expectRevert(encodeError("InsufficientAAmount()"));
+        (uint256 amountA, uint256 amountB, uint256 liquidity) = router
+        .addLiquidity(address(tokenA), address(tokenB), 2 ether, 0.9 ether, 2 ether, 1 ether, address(this));
+    }
+
+    function testAddLiquidityAmountBOptimalIsTooHighAmountAOk() public {
+        HySwapPair pair = HySwapPair(factory.createPair(address(tokenA), address(tokenB)));
+
+        assertEq(pair.token0(), address(tokenA));
+        assertEq(pair.token1(), address(tokenB));
+
+        tokenA.transfer(address(pair), 10 ether);
+        tokenB.transfer(address(pair), 5 ether);
+        uint256 _liquidity = pair.mint(address(this));
+
+        tokenA.approve(address(router), 2 ether);
+        tokenB.approve(address(router), 1 ether);
+
+        (uint256 amountA, uint256 amountB, uint256 liquidity) = router
+        .addLiquidity(address(tokenA), address(tokenB), 2 ether, 0.9 ether, 1.7 ether, 1 ether, address(this));
+
+        assertEq(amountA, 1.8 ether);
+        assertEq(amountB, 0.9 ether);
+        assertEq(liquidity, 1272792206135785543);
+    }
+
+
 }
